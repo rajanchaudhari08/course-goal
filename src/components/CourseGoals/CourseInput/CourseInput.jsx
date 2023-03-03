b37e70c050612ce6a76383e050bada4c89145b6e
Rajan Chaudhari <rajanchaudharigits@gmail.com>
2023-03-03 20:57:09 +0530

    Using Conditional Inline CSS

--- src/components/CourseGoals/CourseInput/CourseInput.jsx
+++ src/components/CourseGoals/CourseInput/CourseInput.jsx
@@ -4,21 +4,35 @@ import "./CourseInput.css";
 
 const CourseInput = (properties) => {
   const [inputValue, setInputValue] = useState("");
+  const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
     setInputValue(event.target.value);
   };
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
+    if (inputValue.trim().length === 0) {
+      // console.log("string.length is empty");
+      setIsValid(false);
+      return;
+    }
     properties.onAddGoal(inputValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={`form-control ${!isValid ? "invalid" : ""}`}>
         <label>Set Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <input
+        /* Conditional Inline CSS */
+          // style={{ borderColor: !isValid ? "red" : "grey" }}
+          type="text"
+          onChange={goalInputChangeHandler}
+        />
       </div>
       <Button type="submit">Submit Goal</Button>
     </form>
